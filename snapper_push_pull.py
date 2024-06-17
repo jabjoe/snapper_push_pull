84bbd969f4c5b0db8806c52c76404fee52ea05c9
Joe Burmeister <joe.burmeister@devtank.co.uk>
2024-06-17 21:03:28 +0100

    PEP8 changes.

--- snapper_push_pull.py
+++ snapper_push_pull.py
@@ -6,12 +6,15 @@ import argparse
 import logging
 import string
 from pathlib import Path
-from collections import namedtuple 
+from collections import namedtuple
 
 parser = argparse.ArgumentParser("Snapper_push_pull")
-parser.add_argument('src', metavar='src_path', type=str, nargs='?', help='snapper controlled source directory: /mnt/mylocal/@snaps or root@remote:/mnt/backup/@snaps')
-parser.add_argument('dst', metavar='dst_path', type=str, nargs='?', help='destination directory: /mnt/mylocal/@snaps or root@remote:/mnt/backup/@snaps')
-parser.add_argument('--dryrun', help="Don't acturally do it.", action='store_true')
+parser.add_argument('src', metavar='src_path', type=str, nargs='?',
+                    help='snapper controlled source directory: /mnt/mylocal/@snaps or root@remote:/mnt/backup/@snaps')
+parser.add_argument('dst', metavar='dst_path', type=str, nargs='?',
+                    help='destination directory: /mnt/mylocal/@snaps or root@remote:/mnt/backup/@snaps')
+parser.add_argument(
+    '--dryrun', help="Don't acturally do it.", action='store_true')
 parser.add_argument('-v', '--verbose', help="Info log.", action='store_true')
 parser.add_argument('--debug', help="Debug log.", action='store_true')
 
@@ -21,6 +24,7 @@ logger = logging.getLogger("snapper_push_push")
 subv_t = namedtuple('subv_t', ['id', 'path', 'uuid'])
 
 dryrun = False
+
 
 class subv_map_t:
     def __init__(self):
@@ -48,9 +52,9 @@ class subv_map_t:
                     continue
                 uuid_parts = uuid.split('-')
                 assert len(uuid) == 36 and \
-                       len(uuid_parts) == 5 and \
-                       min(map(lambda s : min([ c in string.hexdigits for c in s]), [ p for p in uuid_parts ])),\
-                       "Columnn 10 was not a Btrfs UUID"
+                    len(uuid_parts) == 5 and \
+                    min(map(lambda s: min([c in string.hexdigits for c in s]), [p for p in uuid_parts])), \
+                    "Columnn 10 was not a Btrfs UUID"
                 if part_parts[-1] == "snapshot":
                     try:
                         snapper_id = int(part_parts[-2])
@@ -62,26 +66,26 @@ class subv_map_t:
                                         uuid))
 
     def get_mismatches(self, targets):
-        r =[]
+        r = []
         for path, subv in targets.paths.items():
             ref = self.paths.get(path)
             if ref:
                 if ref.id != subv.id or ref.uuid != subv.uuid:
                     logger.error(f"Bad match {subv} {ref}")
-                    r+=[subv]
+                    r += [subv]
             else:
                 logger.info(f"No match for {subv}")
-                r+=[subv]
+                r += [subv]
         for subv in r:
             targets.remove(subv)
         return r
- 
+
     def get_matches(self, targets):
         r = []
         for path, subv in targets.paths.items():
             ref = self.paths.get(path)
             if ref and ref.uuid == subv.uuid and ref.id == subv.id:
-                r+=[ref]
+                r += [ref]
         return r
 
 
@@ -120,7 +124,7 @@ class local_btrfs_t:
 
     def _get_subv_map(self, cmd):
         logger.debug(f"CMD: {cmd}")
-        lines=os.popen(cmd).read().split('\n')
+        lines = os.popen(cmd).read().split('\n')
         r = subv_map_t()
         r.from_subv_list(lines)
         return r
@@ -178,7 +182,6 @@ class local_btrfs_t:
             self.set_info_xml(parent_path, info_xml)
 
 
-
 class remote_btrfs_t(local_btrfs_t):
     def __init__(self, user, host, mnt):
         logger.info(f"Remote {user}@{host}")
@@ -214,7 +217,6 @@ class remote_btrfs_t(local_btrfs_t):
         return self._ssh_wrap_cmd(super().set_info_xml_cmd(parent_path, info_xml))
 
 
-
 def get_btrfs(path):
     parts = path.split(':')
     if len(parts) > 1:
@@ -233,8 +235,8 @@ if __name__ == '__main__':
     dryrun = args.dryrun
 
     if not args.src or not args.dst:
-       parser.print_help()
-       exit(-1)
+        parser.print_help()
+        exit(-1)
 
     if args.verbose:
         logging.basicConfig(level=logging.INFO)
@@ -261,12 +263,13 @@ if __name__ == '__main__':
             logger.error("No matches found")
             exit(-1)
 
-        ids = [ subv.id for subv in matches ]
+        ids = [subv.id for subv in matches]
         ids.sort()
 
         highest_match_id = ids[-1]
 
-        new_refs = list(filter(lambda ref : True if ref.id > highest_match_id else False, src_subvs.ids.values()))
+        new_refs = list(filter(lambda ref: True if ref.id >
+                        highest_match_id else False, src_subvs.ids.values()))
         parent_ref = src_subvs.ids[highest_match_id]
     else:
         logger.error("No matching destinations, starting fresh.")
@@ -274,7 +277,7 @@ if __name__ == '__main__':
         ids = list(src_subvs.ids.keys())
         ids.sort()
 
-        new_refs = [ src_subvs.ids[ref_id] for ref_id in ids ]
+        new_refs = [src_subvs.ids[ref_id] for ref_id in ids]
         parent_ref = None
 
     for ref in new_refs:
